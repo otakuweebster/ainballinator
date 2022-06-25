046de5834c0ddf1e5c8be1804da965469ea99561
Gabriel Beltran <gabrielaudbeltran23@gmail.com>
2022-06-25 15:53:53 -0600

    Ready for pre-beta release!

--- src/script/combo_nation_function.js
+++ src/script/combo_nation_function.js
@@ -4,8 +4,8 @@ $(function ()
      * VARIABLES FOR THE FUNCTIONALITY
      */
     let nationArrayIndex = 0;
-    let nationPhotoArray = ["blank", "AIN", "ACADIA", "ALPINE", "AQUILONA" , "BALISCA", "CALARE", "CALIFORNIA", "ESQUIMALT", "GERMANY", "IBERIA", "ILLIUM", "JARRABAN", "KARASEM", "KOREAN", "MALERNO", "MARRASCANIA", "MASYAN", "MEXICO", "NAKAMA", "NEWDUV", "NWF", "OKA","PHILIPPINES","RIO"];
-    let nationArray = ["", "AIN", "Acadia", "Alpine Federation", "Aquilona" , "Balisca", "Calare", "California", "Esquimalt", "Germany", "Iberia", "Illium", "Jarraban", "Karasem", "Korean Empire", "Malerno","Marrascania", "Masyan", "Mexico", "Nakama", "New Duveland", "New Westfalen", "Oka","Philippines", "Río de La Plata"];
+    let nationPhotoArray = ["blank", "AIN", "ACADIA", "ALPINE", "AQUILONA" , "BALISCA", "CALARE", "CALIFORNIA", "ESQUIMALT", "GERMANY", "IBERIA", "ILLIUM", "JARRABAN", "KARASEM", "KOREAN", "MALERNO", "MARRASCANIA", "MASYAN", "MEXICO", "NAKAMA", "NEWDUV", "NWF", "OKA","PHILIPPINES","RIO", "SANSHENG","SICULIA","SONORA","SU","TAK","TEI","US","WP", "ZAM"];
+    let nationArray = ["", "AIN", "Acadia", "Alpine Federation", "Aquilona" , "Balisca", "Calare", "California", "Esquimalt", "Germany", "Iberia", "Illium", "Jarraban", "Karasem", "Korean Empire", "Malerno","Marrascania", "Masyan", "Mexico", "Nakama", "New Duveland", "New Westfalen", "Oka","Philippines", "Río de La Plata","Sansheng","Siculia","Sonora","Soviet Union","Takamaiku","Teiko","United States","West Pacific","Zambezi"];
     let currentNationChoice = "";
     let currentMood = "BLANK";
 
@@ -13,7 +13,18 @@ $(function ()
     const next = $(".nextbtn");
     const photo = $(".combores");
     const nation = $(".countryname");
+    const randomizer = $(".randomize");
     const moodcmb = $(".custom-select");
+
+    /**
+     * REMOVES THE SPLASHINTRO FROM EXISTENCE
+     */
+    setTimeout(function () {
+        $("#loader").slideUp(500, function () {
+            $(this).remove();
+        });
+    },2000)
+
 
     /**
      * THIS BASICALLY GO BACK TO PREVIOUS COUNTRIES
@@ -104,6 +115,35 @@ $(function ()
         }
 
             photo.attr('src', 'src/img/combo/'+ nationPhotoArray[nationArrayIndex] +"_"+ currentMood +'.png');
+    });
+
+    /**
+     * UPON CLICKING THE RANDOM BUTTON, IT RANDOMIZES A COMBO OF NATION AND MOOD AS WELL!.
+     */
+    randomizer.on("click", function ()
+    {
+        let randNation = Math.floor(Math.random() * ((nationPhotoArray.length - 1) - 0 ) + 1);
+        let randMood = Math.floor(Math.random() * (5 - 1) + 1);
+
+        nationArrayIndex = randNation;
+
+        $('.custom-select option:eq('+randMood+')').prop('selected', true);
+        switch (randMood)
+        {
+            case 0: currentMood = "BLANK";
+                break;
+            case 1: currentMood = "NORMAL";
+                break;
+            case 2: currentMood = "HAPPY";
+                break;
+            case 3: currentMood = "ANGRY";
+                break;
+            case 4: currentMood = "SCARED";
+                break;
+        }
+
+        nation.text(nationArray[nationArrayIndex]);
+        photo.attr('src', 'src/img/combo/'+ nationPhotoArray[nationArrayIndex] +"_"+ currentMood +'.png');
     })
 
     /**
